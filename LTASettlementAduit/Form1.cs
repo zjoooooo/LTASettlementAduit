682c92e6d62003602c2d44a1e4a328419d2f9136
zjoooooo@hotmail.com <wei3shen2me>
2018-02-14 12:23:05 +0800

    First Commit

--- LTASettlementAduit/Form1.cs
+++ LTASettlementAduit/Form1.cs
@@ -16,5 +16,18 @@ namespace LTASettlementAduit
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CollectData()
+        {
+
+
+
+
+        }
     }
 }
